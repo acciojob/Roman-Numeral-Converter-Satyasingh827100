b69266b17a3a9df57deed5da7d6897d96831330c
satya singh <150156041+Satyasingh827100@users.noreply.github.com>
2024-01-15 14:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ function convertToRoman(num) {
 
   return romanNumeral;
 }
-console.log(convertToRoman(14)); 
-console.log(convertToRoman(798));
+//console.log(convertToRoman(14)); 
+//console.log(convertToRoman(798));
  
